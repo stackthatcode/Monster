7706e981866012440ea9f718940d0a9b5a09329a
stackthatcode <aleksjones@gmail.com>
2019-02-20 08:02:00 -0600

    Inventory Sync Control - grid layout

--- Database/Play/Sync Control Buildout.sql
+++ Database/Play/Sync Control Buildout.sql
@@ -1,13 +1,12 @@
 
 SELECT * FROM usrSystemState;
 
-UPDATE usrSystemState SET InventoryPull = 1;
+UPDATE usrSystemState SET InventoryPull = 2;
 
 SELECT * FROM vw_SyncVariantsAndStockItems;
 
 SELECT * FROM usrShopifyVariant;
 
+SELECT * FROM vw_SyncVariantAndStockItem;
 
-SELECT * FROM vw_SyncShopifyInventory;
 
-ezz
