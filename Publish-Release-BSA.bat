2762f39a473fb3878c0c53373022d1d1e2eb4b0e
Aleksandr Jones <aleks@logicautomated.com>
2020-03-04 18:37:10 -0600

    Again, minor build/deploy process tweaks

--- Publish-Release-BSA.bat
+++ Publish-Release-BSA.bat
@@ -1,10 +1,10 @@
 CD C:\DEV\Monster
 
 REM clean-up the output directories
-rmdir /S /Q "C:\DEV\Monster\BSA01-Deployment"
-mkdir "C:\DEV\Monster\BSA01-Deployment"
-mkdir "C:\DEV\Monster\BSA01-Deployment\ConsoleApp"
-mkdir "C:\DEV\Monster\BSA01-Deployment\WebApp"
+rmdir /S /Q "C:\DEV\Monster\BSA-Deployment"
+mkdir "C:\DEV\Monster\BSA-Deployment"
+mkdir "C:\DEV\Monster\BSA-Deployment\ConsoleApp"
+mkdir "C:\DEV\Monster\BSA-Deployment\WebApp"
 REM pause
 
 REM build the solution
@@ -15,6 +15,6 @@ REM this step publishes the Web App
 msbuild C:\DEV\Monster\Monster.Web\Monster.Web.csproj  /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
 
 REM this step publishes (copies) the Console App
-xcopy /e "C:\DEV\Monster\Monster.Console\bin\Release" "C:\DEV\Monster\BSA01-Deployment\ConsoleApp\"
+xcopy /e "C:\DEV\Monster\Monster.Console\bin\Release" "C:\DEV\Monster\BSA-Deployment\ConsoleApp\"
 
 pause
